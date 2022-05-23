e6b1ad6122876801f648f9d9284fd0b829c505ac
Tan Hui Xin/NP <92862867+HuiXin681@users.noreply.github.com>
2022-05-23 20:51:41 +0800

    Initialize labels for the ui for the user object

--- app/src/main/java/sg/np/edu/mad/exercise2/MainActivity.java
+++ app/src/main/java/sg/np/edu/mad/exercise2/MainActivity.java
@@ -7,11 +7,17 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    User = new newUser { "Hui Xin", "It student", 1, flase}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        TextView name = findViewById(R.id.name);
+        name.setText(newUser.name);
+        
+        TextView desc = findViewById(R.id.fillerText);
+        desc.setText(newUser.desc);
     }
 
     public void followUnfollow(View view){
